bd6e17c3e76c17dd0a1dc56fe0a6326369d6ee62
Asaf Dulman <asafdulman@gmail.com>
2021-03-21 20:02:02 +0200

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
 import { Footer } from "./cmps/Footer";
 import { Hero } from "./cmps/Hero";
 import { NavBar } from "./cmps/NavBar";
-import logo from '../src/assets/img/logo.png'
-import hey from '../src/assets/img/Shortly.svg'
 
 function App() {
   return (
     <div className="app">
-      <h1>hey man</h1>
-      <img src={logo} alt=""/>
-      <img src={hey} alt=""/>
+      <h3>check agian</h3>
       <NavBar />
       <Hero />
 
